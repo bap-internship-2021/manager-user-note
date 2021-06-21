448c17556bcc94c65c7dada006558a225a373496
ngocphuocha <phuoc04012000@gmail.com>
2021-06-21 17:05:04 +0700

    restore file from gitignore

---
+++ controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+require './models/User.php';
+
+class UserController extends User
+{
+    public function handleLogin()
+    {
+        // Get input from users ....
+        $email = trim(filter_input(INPUT_POST, "email"));
+        $password = trim(filter_input(INPUT_POST, "password"));
+//        $email = trim($email);
+//        $password = trim($password);
+        return $this->login($email, $password); // Call login from User model
+    }
+}
